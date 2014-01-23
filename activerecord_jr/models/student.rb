683164287c6fdf359a0eced3cbf350f39d1bc624
Christina Taggart & Ricardo A. Rubio <pair+christina-taggart.rickarubio@devbootcamp.com>
2014-01-22 16:01:39 -0800

    Release 0: Refactor SQL Into the Base Class

---
+++ activerecord_jr/models/student.rb
@@ -0,0 +1,22 @@
+class Student < Database::Model
+
+  self.attribute_names =  [:id, :cohort_id, :first_name, :last_name, :email,
+                           :gender, :birthdate, :created_at, :updated_at]
+
+  # We say a record is "new" if it doesn't have a defined primary key in its
+  # attributes
+  # def new_record?
+  #   self[:id].nil?
+  # end
+
+  def cohort
+    Cohort.where('id = ?', self[:cohort_id]).first
+  end
+
+  def cohort=(cohort)
+    self[:cohort_id] = cohort[:id]
+    self.save
+    cohort
+  end
+
+end
